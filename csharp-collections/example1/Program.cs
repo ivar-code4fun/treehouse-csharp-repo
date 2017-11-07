e3b0260d997d5e045bf82a93286214e99b79ce5d
ravishankar <ravishankar.r1@gmail.com>
2017-11-06 22:22:08 -0800

    treehouse csharp-repo initial commit

---
+++ csharp-collections/example1/Program.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Treehouse
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student() { Name = "Sally" , GradeLevel = 3 },
+                new Student() { Name = "Bob" , GradeLevel = 3 },
+                new Student() { Name = "Sally" , GradeLevel = 2 }
+            };
+          
+            students.Sort();
+          
+            Student newStudent = new Student() { Name = "Joe", GradeLevel = 2 };
+            
+            int index = students.BinarySearch(newStudent);
+            if (index < 0)
+            {
+                students.Insert(~index, newStudent);
+            }
+            
+         
+            foreach (Student student in students)
+            {
+                System.Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
+            }
+        }
+    }
+}
