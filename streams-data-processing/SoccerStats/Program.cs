266f05cb18b2eaf7acd713882bfe561d71909715
ravishankar <ravishankar.r1@gmail.com>
2017-11-06 23:21:03 -0800

    reading a csv file

--- streams-data-processing/SoccerStats/Program.cs
+++ streams-data-processing/SoccerStats/Program.cs
@@ -14,11 +14,41 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
-            var files = directory.GetFiles();
+            //var files = directory.GetFiles("*.txt");
 
-            foreach (var file in files)
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine(file.Name);
+            //}
+            //var fileName = Path.Combine(directory.FullName, "data.txt");
+            //var file = new FileInfo(fileName);
+            //if (file.Exists)
+            //{
+            //    using (var reader = new StreamReader(file.FullName))
+            //    {
+            //        Console.SetIn(reader);
+            //        Console.WriteLine(Console.ReadLine());
+            //    }
+
+            //    Console.ReadLine();
+            //}
+
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in fileLines)
             {
-                Console.WriteLine(file.Name);
+                Console.WriteLine(line);
+            }
+
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
             }
         }
     }
