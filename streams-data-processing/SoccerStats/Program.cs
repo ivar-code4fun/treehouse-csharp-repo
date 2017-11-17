12486d0072d828ee3b08e9948d671fffdc5c7dc2
ravishankar <ravishankar.r1@gmail.com>
2017-11-16 22:32:51 -0800

    Soccer Stats Project

--- streams-data-processing/SoccerStats/Program.cs
+++ streams-data-processing/SoccerStats/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace SoccerStats
 {
@@ -34,13 +36,34 @@ namespace SoccerStats
             //}
 
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //var fileContents = ReadFile(fileName);
+            //string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (var line in fileLines)
+            //foreach (var line in fileLines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+            var fileContents = ReadSoccerResults(fileName);
+
+            var playerFileName = Path.Combine(directory.FullName, "players.json");
+            var players = DeserializePlayers(playerFileName);
+
+            var topTenPlayers = GetTopTenPlayer(players);
+
+            //foreach (var player in players)
+            //{
+            //    Console.WriteLine(player.FirstName);
+            //}
+
+            foreach (var player in topTenPlayers)
             {
-                Console.WriteLine(line);
+                Console.WriteLine("Name : " + player.FirstName + " PPG : " + player.PointsPerGame);
             }
+
+            fileName = Path.Combine(directory.FullName, "topten.json");
+            SerializePlayersToFile(topTenPlayers, fileName);
+
+            Console.WriteLine(GetGoogleHomePage());
 
         }
 
@@ -51,5 +74,134 @@ namespace SoccerStats
                 return reader.ReadToEnd();
             }
         }
+
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<GameResult>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+                    
+                    DateTime gameDate;
+                    if (DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+
+                    double possesionPercent;
+                    if (double.TryParse(values[7], out possesionPercent))
+                    {
+                        gameResult.PossesionPercent = possesionPercent;
+                    }
+
+                    soccerResults.Add(gameResult);
+                }
+            }
+
+            return soccerResults;
+        }
+
+        public static List<Player> DeserializePlayers(string fileName)
+        {
+            var players = new List<Player>();
+
+            var serializer = new JsonSerializer();
+
+            using (var reader = new StreamReader(fileName))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                players = serializer.Deserialize<List<Player>>(jsonReader);
+            }
+
+
+            return players;
+        }
+
+        public static List<Player> GetTopTenPlayer(List<Player> players)
+        {
+            var topTenPlayers = new List<Player>();
+            players.Sort(new PlayerComparer());
+            int counter = 0;
+
+            foreach (var player in players)
+            {
+                topTenPlayers.Add(player);
+                counter++;
+
+                if (counter == 10)
+                    break;
+            }
+
+            return topTenPlayers;
+        }
+
+        public static void SerializePlayersToFile(List<Player> players,string fileName)
+        {
+            var serializer = new JsonSerializer();
+
+            using (var writer = new StreamWriter(fileName))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                serializer.Serialize(jsonWriter, players);
+            }
+
+        }
+
+        public static string GetGoogleHomePage()
+        {
+            var webClient = new WebClient();
+            byte[] googleHome = webClient.DownloadData("https://www.google.com");
+
+            using (var stream = new MemoryStream(googleHome))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static string GetNewsForPlayer(string playerName)
+        {
+            var webClient = new WebClient();
+            webClient.Headers.Add("key","value");
+            byte[] searchResults = webClient.DownloadData(string.Format("https://bingapis.azure-api.net/api/v5/news/search?q={0}&mkt=en-us",playerName));
+
+            using (var stream = new MemoryStream(searchResults))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
     }
 }
